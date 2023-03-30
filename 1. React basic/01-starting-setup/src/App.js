a52608c572f3080954c6a9e3d3485df177b57add
Deva <theprojessa@gmail.com>
2023-03-30 21:09:57 +0700

    wrapper / children component

--- 1. React basic/01-starting-setup/src/App.js
+++ 1. React basic/01-starting-setup/src/App.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
 
-  const expense = [
+  const expenses = [
     {
       'id' : 'e1',
       'title' : 'Coffe',
@@ -32,29 +32,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-      title={expense[0].title}
-      date={expense[0].date}
-      amount={expense[0].amount}
-      ></ExpenseItem>
-
-      <ExpenseItem
-      title={expense[1].title}
-      date={expense[1].date}
-      amount={expense[1].amount}
-      ></ExpenseItem>
-
-      <ExpenseItem
-      title={expense[2].title}
-      date={expense[2].date}
-      amount={expense[2].amount}
-      ></ExpenseItem>
-
-      <ExpenseItem
-      title={expense[3].title}
-      date={expense[3].date}
-      amount={expense[3].amount}
-      ></ExpenseItem>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
