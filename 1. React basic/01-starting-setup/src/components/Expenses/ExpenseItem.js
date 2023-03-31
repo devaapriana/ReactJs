5e6eb01b2b2195ba8a6a9458685f8627c375e06b
Deva <theprojessa@gmail.com>
2023-04-01 02:38:42 +0700

    event handler

--- 1. React basic/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 1. React basic/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,11 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 function ExpenseItem(props){
+
+    const clickHandler = () => {
+        alert('Clicked!!');
+    }
 
     return (
         <Card className='expense-item'>
@@ -10,6 +14,7 @@ function ExpenseItem(props){
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
         );   
     }
