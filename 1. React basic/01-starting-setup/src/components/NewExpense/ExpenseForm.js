16cc08115864dcd640e3cb747f31b7411caace80
Deva <theprojessa@gmail.com>
2023-04-01 22:54:12 +0700

    multi state & form input

---
+++ 1. React basic/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+
+import './ExpenseForm.css';
+const ExpenseForm = () => {
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    const changeTitleHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+
+    const changeAmountHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    }
+
+    const changeDateHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
+  
+
+    return (
+        <form>
+        <div className="new-expense__controls">
+            <div className="new-expense__control">
+                <label>Title</label>
+                <input type='text' onChange={changeTitleHandler}/>
+            </div>
+            <div className="new-expense__control">
+                <label>Amount</label>
+                <input type='number' onChange={changeAmountHandler} min='0.01' step='0.01' />
+            </div>
+            <div className="new-expense__control">
+                <label>Date</label>
+                <input type='date' onChange={changeDateHandler} min='2020-01-01' step='2023-12-31' />
+            </div>
+            <div className='new-expense__actions'>
+                <button>Add Expense</button>
+            </div>
+        </div>
+        </form>
+    )
+}
+
+export default ExpenseForm;
