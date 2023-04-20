6e55742f5a62af9a79755bc76984d1194854fc65
Deva <theprojessa@gmail.com>
2023-04-21 02:37:10 +0700

    refactor react context

--- Intermediate/01-useEffect/src/store/auth-context.js
+++ Intermediate/01-useEffect/src/store/auth-context.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {},
+    onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  
+    useEffect(()=>{
+        const isLoginData = localStorage.getItem('isLogin');
+
+        if(isLoginData === '1'){
+        setIsLoggedIn(true);
+        }
+    }, []);
+  
+  
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLogin', '1');
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLogin');
+        setIsLoggedIn(false);
+    };
+
+
+    return <AuthContext.Provider value={{isLoggedIn: isLoggedIn, onLogin: loginHandler, onLogout:logoutHandler}}>
+        {props.children}
+    </AuthContext.Provider>
+}
 
 export default AuthContext;
