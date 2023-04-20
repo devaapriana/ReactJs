1ed41c7f32a5ed4f5a38e2100c3f6f264160087c
Deva <theprojessa@gmail.com>
2023-04-20 15:19:55 +0700

    useContext() hook

--- Intermediate/01-useEffect/src/components/MainHeader/Navigation.js
+++ Intermediate/01-useEffect/src/components/MainHeader/Navigation.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+
+  const ctx = useContext(AuthContext);
+
   return (
 
-    <AuthContext.Consumer>
-      {(ctx)=>{
-        return (
-          <nav className={classes.nav}>
+      <nav className={classes.nav}>
       <ul>
         {ctx.isLoggedIn && (
           <li>
@@ -28,10 +28,7 @@ const Navigation = (props) => {
         )}
       </ul>
     </nav>
-        )
-      }}
-    
-    </AuthContext.Consumer>
+   
   );
 };
 
