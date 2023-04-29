3a11326b2c10c505b9d137869ad2d05350060275
Deva <theprojessa@gmail.com>
2023-04-29 17:00:54 +0700

    manage cart context with useReducer

--- React-meals-app/src/store/CartProvider.js
+++ React-meals-app/src/store/CartProvider.js
@@ -1,11 +1,35 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartItems = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if(action.type === 'ADD'){
+        const updatedItems = state.items.concat(action.item);
+        const newTotalAmount = state.totalAmount + action.amount * action.price;
+
+        return {
+            items: updatedItems,
+            totalAmount: newTotalAmount
+        };
+    }
+    return defaultCartItems;
+}
 
 const CartProvider = props => {
 
-    const addItemToCartHandler = () => {};
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartItems);
+
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({type: 'ADD', item: item});
+    };
     
-    const removeItemFromCartHandler = () => {};
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({type: 'REMOVE', id: id});
+    };
 
     const cartContext = {
         items: [],
