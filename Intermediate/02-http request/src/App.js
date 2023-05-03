c796056f47e6e8c71db6faf112a35ca8418c6bc4
Deva <theprojessa@gmail.com>
2023-05-03 15:40:56 +0700

    handle side effect http request with useEffect

--- Intermediate/02-http request/src/App.js
+++ Intermediate/02-http request/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler () {
+  const fetchMoviesHandler = useCallback(async function  () {
     setIsLoading(true);
     setError(null);
 
@@ -40,9 +40,12 @@ function App() {
       }
       
       setIsLoading(false);
-    
+  
+  }, []);
 
-  }
+  useEffect(()=>{
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies.</p>
 
