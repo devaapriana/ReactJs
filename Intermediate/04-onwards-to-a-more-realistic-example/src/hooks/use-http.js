631e412a7960a2d2a0186727ed9c187d06c68498
Deva <theprojessa@gmail.com>
2023-05-06 03:15:40 +0700

    building a custom hook

---
+++ Intermediate/04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
@@ -0,0 +1,50 @@
+const useHttp = (requestConfig, applyData) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
+    const sendRequest = async (taskText) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          requestConfig.url,
+          {
+            method: requestConfig.method,
+            body: JSON.stringify(requestConfig.body),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+  
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+  
+        const data = await response.json();
+  
+        const loadedTasks = [];
+
+        for (const taskKey in data) {
+            loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+        }
+
+         applyData(data);
+
+        } catch (err) {
+
+            setError(err.message || 'Something went wrong!');
+
+        }
+
+        setIsLoading(false);
+    };
+
+    return {
+        isLoading,
+        error,
+        sendRequest
+    }
+}
+
+export default useHttp;
