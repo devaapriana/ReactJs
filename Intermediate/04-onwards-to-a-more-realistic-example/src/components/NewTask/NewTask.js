a72265bdacf4d6deef987af39065f494568820ad
Deva <theprojessa@gmail.com>
2023-05-06 04:35:45 +0700

    using the custom hook in more component

--- Intermediate/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
+++ Intermediate/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
@@ -1,42 +1,28 @@
 import { useState } from 'react';
+import useHttp from '../../hooks/use-http';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-app-610ea-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+  const {isLoading, error, sendRequest: sendTaksRequest} = useHttp();
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
+  const addTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
       const createdTask = { id: generatedId, text: taskText };
 
       props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+  }
+
+  const enterTaskHandler = async (taskText) => {
+
+    sendTaksRequest({url: 'https://react-app-610ea-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json', method: 'POST', headers: {
+      'Content-Type': 'application/json',
+    }, body: JSON.stringify({ text: taskText })}, addTask.bind(null, taskText));
+
   };
+   
 
   return (
     <Section>
