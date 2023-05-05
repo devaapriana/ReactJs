79178208d16cbe126f8075e15825d9b6f3377b2d
Deva <theprojessa@gmail.com>
2023-05-06 04:10:34 +0700

    adjusting the custom hook logic

--- Intermediate/04-onwards-to-a-more-realistic-example/src/App.js
+++ Intermediate/04-onwards-to-a-more-realistic-example/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -9,7 +9,7 @@ function App() {
   
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (data) => {
+  const transformTasks = useCallback((data) => {
     const loadedTasks = [];
 
       for (const taskKey in data) {
@@ -17,15 +17,16 @@ function App() {
       }
 
       setTasks(loadedTasks);
-  }
+      
+  }, []);
 
-  const {isLoading, error, sendRequest: fetchTasks} = useHttp({url: 'https://react-app-610ea-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'}, transformTasks)
+  const {isLoading, error, sendRequest: fetchTasks} = useHttp()
 
 
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({url: 'https://react-app-610ea-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'}, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- Intermediate/04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
+++ Intermediate/04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
   
-    const sendRequest = async (taskText) => {
+    const sendRequest = useCallback(async (requestConfig, applyData) => {
       setIsLoading(true);
       setError(null);
       try {
@@ -38,7 +38,7 @@ const useHttp = (requestConfig, applyData) => {
         }
 
         setIsLoading(false);
-    };
+    }, []);
 
     return {
         isLoading,
