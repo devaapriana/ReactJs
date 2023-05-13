9a081b6fede68ec0928729fde160f7186a15b44d
Deva <theprojessa@gmail.com>
2023-05-13 22:45:13 +0700

    dispatch an action in redux toolkit

--- Intermediate/07-react-redux/src/store/index.js
+++ Intermediate/07-react-redux/src/store/index.js
@@ -16,7 +16,7 @@ const counterSlice = createSlice({
             state.counter--;
         },
         increase(state, action){
-            state.counter = state.counter + action.amount;
+            state.counter = state.counter + action.payload;
         },
         toggleCounter(state){
             state.showCounter = !state.showCounter;
@@ -57,8 +57,11 @@ const counterReducer = (state = initialState, action) => {
     
     return state;
 }
+
 const store = configureStore({
-    reducer: counterSlice.reducer
+    counter: counterSlice.reducer
 })
+
+export const counterActions = counterSlice.actions;
 
 export default store;
