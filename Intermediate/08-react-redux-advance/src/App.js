4e2392e84569ad472efa89dbfff31ab0f25df2f8
Deva <theprojessa@gmail.com>
2023-05-15 21:37:54 +0700

    using useEffect with redux

--- Intermediate/08-react-redux-advance/src/App.js
+++ Intermediate/08-react-redux-advance/src/App.js
@@ -1,11 +1,21 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  const cart = useSelector(state => state.cart);
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
-  
+  useEffect(()=>{
+    fetch('https://react-app-610ea-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+      method: 'PUT', 
+      body: JSON.stringify(cart),
+      headers: {
+        'Content-Type': 'Application/JSON'
+      }
+    });
+  }, [cart]);
   return (
     <Layout>
       {!cartIsVisible && <Cart />}
