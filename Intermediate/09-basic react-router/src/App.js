5bd7524bbb8370f64f5568e86730491153438b63
Deva <theprojessa@gmail.com>
2023-06-20 16:21:35 +0700

    layout and nested route

--- Intermediate/09-basic react-router/src/App.js
+++ Intermediate/09-basic react-router/src/App.js
@@ -1,10 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import Root from "./pages/Root";
 
 const router = createBrowserRouter([
-  {path: '/', element: <Home />},
-  {path: '/product', element: <Product />},
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {path: '/', element: <Home />},
+      {path: '/product', element: <Product />}
+    ]
+  }
+  
 ])
 
 function App() {
