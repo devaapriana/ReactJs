42b172c79f793099c9110153efb1b1570976330c
Deva <theprojessa@gmail.com>
2023-06-21 04:25:45 +0700

    navigating programmatically

--- Intermediate/09-basic react-router/src/pages/Home.js
+++ Intermediate/09-basic react-router/src/pages/Home.js
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const navigateHandler = () => {
+        navigate('/product');
+    }
+
     return <>
     <h1>Hello World</h1>
     <p>
         Go to <Link to='/product'>list product</Link>
+    </p>
+    <p>
+        <button onClick={navigateHandler}>Navigate</button>
     </p>
     </>
 }
