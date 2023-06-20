1102d204f8bfda7764bdc0da1e5e2ab1f5fba770
Deva <theprojessa@gmail.com>
2023-06-21 04:09:00 +0700

    showing error page

--- Intermediate/09-basic react-router/src/App.js
+++ Intermediate/09-basic react-router/src/App.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Root from "./pages/Root";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage/>,
     children: [
       {path: '/', element: <Home />},
       {path: '/product', element: <Product />}
---
+++ Intermediate/09-basic react-router/src/pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import MainNavigation from "../components/MainNavigation";
+
+const ErrorPage = () => {
+    return <>
+        <MainNavigation/>
+        <main>
+            <h1>Error Page 404</h1>
+            <p>Page not found</p>
+        </main>   
+    </>
+}
+
+export default ErrorPage;
