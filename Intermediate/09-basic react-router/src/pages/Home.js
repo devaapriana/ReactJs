7f8f512f43a6f6aa0219a2892e5ac0b58b5f619e
Deva <theprojessa@gmail.com>
2023-06-20 14:34:05 +0700

    navigating between pages with link

--- Intermediate/09-basic react-router/src/pages/Home.js
+++ Intermediate/09-basic react-router/src/pages/Home.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-    return <h1>Hello World</h1>
+    return <>
+    <h1>Hello World</h1>
+    <p>
+        Go to <Link to='/product'>list product</Link>
+    </p>
+    </>
 }
 
 export default Home;
