e49c1d0d6f7a2343f679597701869ec8c68eecc7
Deva <theprojessa@gmail.com>
2023-06-21 14:32:55 +0700

    add link for dynamic route

--- Intermediate/09-basic react-router/src/pages/Product.js
+++ Intermediate/09-basic react-router/src/pages/Product.js
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_PRODUCTS = [
+    {id: 'p1', name: 'Product 1'},
+    {id: 'p2', name: 'Product 2'},
+    {id: 'p3', name: 'Product 3'},
+]
 const Product = () => {
-    return <h1>Product pages</h1>
+    return <>
+    <h1>Product pages</h1>
+    <ul>
+    {DUMMY_PRODUCTS.map(prod => <li key={prod.id}><Link to={`/product/${prod.id}`}>{prod.name}</Link></li>)}
+    </ul>
+    </> 
 }
 
 export default Product;
