1e95cd7031bccbad8fe1973b8d4a76b6f77fae4f
Deva <theprojessa@gmail.com>
2023-08-31 00:27:34 +0700

    managing the token expiration

--- Intermediate/11-authentication/frontend/src/pages/Root.js
+++ Intermediate/11-authentication/frontend/src/pages/Root.js
@@ -1,10 +1,31 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useNavigation, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData()
+  const submit = useSubmit();
+  useEffect(()=>{
+    
+    if(!token){
+      return;
+    }
 
+    if(token === 'EXPIRED'){
+      submit(null, {action: '/logout', method: 'post'});
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    submit(null, {action: '/logout', method: 'post'});
+    setTimeout(()=>{
+    },tokenDuration)
+
+  }, [token])
   return (
     <>
       <MainNavigation />
